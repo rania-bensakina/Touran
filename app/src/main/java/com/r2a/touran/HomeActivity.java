408924fbdea64e03f829789d02e3b4c6aa83c5bb
bensakina <rbensakina16@gmail.com>
2022-01-27 14:58:53 +0100

    fixed tiny bugs

--- app/src/main/java/com/r2a/touran/HomeActivity.java
+++ app/src/main/java/com/r2a/touran/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.r2a.touran;
 
+import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import androidx.annotation.RequiresApi;
@@ -22,7 +25,7 @@ public class HomeActivity extends AppCompatActivity {
     ActivtyHome2Binding binding ;
     Fragment lastFragment;
 
- /*   @Override
+  @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
@@ -34,10 +37,10 @@ public class HomeActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
-                return super.dispatchTouchEvent(ev);
-            }
 
-        }}*/
+            }}
+      return super.dispatchTouchEvent(event);
+  }
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
