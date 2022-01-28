c27c6bc237bbedcdc321e01e1a911ef49a34fabe
bensakina <rbensakina16@gmail.com>
2022-01-28 14:54:57 +0100

    fixed some bugs

--- app/src/main/java/com/r2a/touran/MapsFragment.java
+++ app/src/main/java/com/r2a/touran/MapsFragment.java
@@ -2,25 +2,71 @@ package com.r2a.touran;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.audiofx.Equalizer;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executor;
 
 
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
-public class MapsFragment extends Fragment {
-    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
 
+    private static final String TAG = MapsFragment.class.getSimpleName();
+    private GoogleMap map;
+    private CameraPosition cameraPosition;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
+    private Location lastKnownLocation;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+    /*private final OnMapReadyCallback callback = new OnMapReadyCallback() {
         /**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
@@ -29,21 +75,106 @@ public class MapsFragment extends Fragment {
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
-         */
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
-    };
-
+    };*/
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        View v = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        // Build the map.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (locationPermissionGranted) {
+            getCurrentLocation();
+        }else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+        }
+        return v;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==100 && grantResults.length>0 && grantResults[0]+ grantResults[1]== PackageManager.PERMISSION_GRANTED){
+            getCurrentLocation();
+        }else   Toast.makeText(getContext(), "GIVE ME ACCESS", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getCurrentLocation() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        System.out.println(location.getLongitude());
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        System.out.println(latLng);
+                        //ur object map is null
+                        map.addMarker(new MarkerOptions().position(latLng).title("Ma location"));
+                        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f));
+                    } else {
+                        LocationRequest locationRequest = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(10000).setFastestInterval(1000).setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+
+                                Location location1 = locationResult.getLastLocation();
+                                System.out.println(location1);
+                                LatLng latLng = new LatLng(location1.getLatitude(), location.getLongitude());
+                                map.addMarker(new MarkerOptions().position(latLng).title("Ma location"));
+                                map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                            }
+                        };
+                        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                }
+            }});
+
+        }else {
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+
     }
 
     @Override
@@ -51,8 +182,45 @@ public class MapsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+        }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (map != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, map.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        this.map = map;
+        getLocationPermission();
+
+
+
+    }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
+
 }
+
--- app/src/main/java/com/r2a/touran/PlaceInfoActivity.java
+++ app/src/main/java/com/r2a/touran/PlaceInfoActivity.java
@@ -10,9 +10,12 @@ import androidx.databinding.DataBindingUtil;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -23,6 +26,7 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -30,8 +34,7 @@ import com.r2a.touran.data.Place;
 import com.r2a.touran.databinding.PlaceInfoActivityBinding;
 
 
-public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener{
+public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCallback{
     GoogleMap map;
     Place place;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
@@ -46,7 +49,7 @@ public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCa
             Bundle extras = this.getIntent().getExtras();
             binding.description.setText(extras.getString("name"));
             String price = extras.getString("descrition");
-            Place place = new Place("gyguy",SHOPPING,35.693403,-0.626094);
+             place = new Place("gyguy",SHOPPING,35.693403,-0.626094);
         } else System.out.println("NO INTENT");
 
 
@@ -66,15 +69,6 @@ public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCa
         if (map != null) enableMyLocation();
     }
 
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-    }
-
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -107,20 +101,19 @@ public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCa
         }
     }
     Location pharmaplace ;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void getPlacePosition(Place p) {
         try {
-            @SuppressLint("UseCompatLoadingForDrawables") BitmapDrawable bitmapDrawable = (BitmapDrawable) getResources().getDrawable(R.mipmap.pharmapin, null);
-            Bitmap b = bitmapDrawable.getBitmap();
-            Bitmap smallmarker = Bitmap.createScaledBitmap(b, 72, 120, true);
-            Toast.makeText(this, p.getLatitude().toString(), Toast.LENGTH_SHORT).show();
+          //  @SuppressLint("UseCompatLoadingForDrawables") BitmapDrawable bitmapDrawable = (BitmapDrawable) getResources().getDrawable(R.drawable., null);
+       //     Bitmap b = bitmapDrawable.getBitmap();
+          //  Bitmap smallmarker = Bitmap.createScaledBitmap(b, 72, 120, true);
+            System.out.println(p);
             pharmaplace.setLatitude(p.getLatitude());
             pharmaplace.setLongitude(p.getLongitude());
             if (pharmaplace != null) {
                 LatLng pha = new LatLng(pharmaplace.getLatitude(), pharmaplace.getLongitude());
                 map.addMarker(new MarkerOptions()
                         .position(pha)
-                        .title(p.getName()).icon(BitmapDescriptorFactory.fromBitmap(smallmarker)));
+                        .title(p.getName()).icon(BitmapFromVector(getApplicationContext(), R.drawable.touran_logo)));
                 //  map.moveCamera(CameraUpdateFactory.newLatLng(pha));
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(pha, 14.0f));
             }
@@ -128,5 +121,26 @@ public class PlaceInfoActivity extends AppCompatActivity implements OnMapReadyCa
             Toast.makeText(this, "Cette pharmacie n'a pas encore ajouté sa position", Toast.LENGTH_SHORT).show();
         }
 
+    }
+    private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
